b18d9d551c3f13a1cd3237ef6e38b98c26da5345
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-01-31 15:50:17 -0600

    Utilización de la librería desde el mísmo proyecto.

---
+++ ToolbarLibrary/app/src/main/java/com/shellcore/android/toolbarlibrary/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.shellcore.android.toolbarlibrary
+
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : com.shellcore.android.toolbarlib.ToolbarActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toolbarToLoad(toolbar as Toolbar)
+    }
+}
