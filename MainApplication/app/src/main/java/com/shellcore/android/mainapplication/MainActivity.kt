1f88d9a68af5729658cc4dd2c884e6a00edfd40b
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-01-31 16:29:36 -0600

    Uso de la biblioteca externa, agregando el archivo aar al projecto principal: File -> New -> New Module... -> Import .jar/.aar package

--- MainApplication/app/src/main/java/com/shellcore/android/mainapplication/MainActivity.kt
+++ MainApplication/app/src/main/java/com/shellcore/android/mainapplication/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.shellcore.android.mainapplication
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import com.shellcore.android.toolbarlib.ToolbarActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ToolbarActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbarToLoad(toolbar as Toolbar)
     }
 }
