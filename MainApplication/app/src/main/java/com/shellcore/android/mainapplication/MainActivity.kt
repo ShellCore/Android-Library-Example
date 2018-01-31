f2bcb159c37fd2553792c8e3bcfcae48ad2452ae
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-01-31 16:18:41 -0600

    Uso de la biblioteca externa, copiando el módulo al proyecto actual: File -> New -> Import Module...

--- MainApplication/app/src/main/java/com/shellcore/android/mainapplication/MainActivity.kt
+++ MainApplication/app/src/main/java/com/shellcore/android/mainapplication/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.shellcore.android.mainapplication
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : com.shellcore.android.toolbarlib.ToolbarActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbarToLoad(toolbar as Toolbar)
     }
 }
---
+++ MainApplication/toolbarlib/src/main/java/com/shellcore/android/toolbarlib/interfaces/ToolbarInterface.kt
@@ -0,0 +1,12 @@
+package com.shellcore.android.toolbarlib.interfaces
+
+import android.support.v7.widget.Toolbar
+
+/**
+ * Created by MOGC on 31/01/2018.
+ */
+interface ToolbarInterface {
+
+    fun toolbarToLoad(toolbar: Toolbar?)
+    fun enableHomeDisplay(value: Boolean)
+}
